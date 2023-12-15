0cf68f7f55d2e2bcb59c13237d936375928aee1e
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-15 12:26:04 -0500

    added license link function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,16 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLink = {
+    MIT: "[MIT](https://choosealicense.com/licenses/mit/)",
+  };
+  if (!license) {
+    return "";
+  } else {
+    return licenseLink[license];
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
